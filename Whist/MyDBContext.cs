ef43dabfccb676193fd7e7e600c4262dd1a9e981
Jonas Bay Andreasen <au616208@uni.au.dk>
2020-04-01 09:30:05 +0200

    Jonas

--- Whist/MyDBContext.cs
+++ Whist/MyDBContext.cs
@@ -14,27 +14,29 @@ namespace Whist
             // For SQLServer file, this is
             ob.UseSqlServer("Data Source=127.0.0.1,1433;Database=XXXX;User ID=SA;Password=SecurePassword1!");
         }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            //GameRoundPlayers
+
+            //GameRounds
+
+            //Games
+
+            // Location
+
+            //GamePlayers
+
+            //Players
+
+            //SoleRoundWinner
+
+            //Types
+
+            //SoleRound
+
+            //NormalRound
+        }
     }
-    protected override void OnModelCreating(ModelBuilder mb)
-    {
-        //GameRoundPlayers
-
-        //GameRounds
-
-        //Games
-
-        // Location
-
-        //GamePlayers
-
-        //Players
-
-        //SoleRoundWinner
-
-        //Types
-
-        //SoleRound
-
-        //NormalRound
-    }
+    
 }
