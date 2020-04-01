95c490106e6d7c99165798041a70dfd52c3a763b
jonasbay <jonasbay@hotmail.dk>
2020-04-01 09:37:31 +0200

    -A: no change

--- Whist/MyDBContext.cs
+++ Whist/MyDBContext.cs
@@ -23,7 +23,7 @@ namespace Whist
 
         //Games
 
-        // Location
+        //Location
 
         //GamePlayers
 
@@ -31,7 +31,7 @@ namespace Whist
 
         //SoleRoundWinner
 
-        //Types
+    //Types
 
         //SoleRound
 
